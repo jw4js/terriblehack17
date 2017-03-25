9811f793e9080e47f9b672f7cf1ef6567e64f5bc
jw4js <jw4js@github.com>
2017-03-25 14:28:15 -0400

    -

--- Android/thacks/app/src/main/java/th/thacks/Main.java
+++ Android/thacks/app/src/main/java/th/thacks/Main.java
@@ -4,6 +4,7 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.content.pm.PackageManager;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -29,10 +30,21 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import android.content.Intent;
+import java.io.File;
+import java.io.IOException;
+import android.support.v7.graphics.Palette;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 import static android.Manifest.permission.READ_CONTACTS;
+import java.lang.System;
 
 /**
  * A login screen that offers login via email/password.
@@ -51,6 +63,48 @@ public class Main extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
                                            @NonNull int[] grantResults) {
+    }
+
+    File image_file = null;
+
+    public void takePicture(View view)
+    {
+        try {
+            image_file = File.createTempFile("img", null);
+        } catch (IOException ignored) {}
+        Intent picture_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        picture_intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY,1);
+        Uri image_uri = Uri.fromFile(image_file);
+        picture_intent.putExtra(MediaStore.EXTRA_OUTPUT,image_uri);
+        startActivityForResult(picture_intent,0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,Intent data)
+    {
+        switch(requestCode)
+        {
+            case 0:
+            {
+                if(resultCode == RESULT_OK)
+                {
+                    BitmapFactory.Options bitmap_options = new BitmapFactory.Options();
+                    bitmap_options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+                    Bitmap image = null;
+                    try {
+                        InputStream is = new FileInputStream(image_file);
+                        image = BitmapFactory.decodeStream(is,null,bitmap_options);
+                        System.out.println(image);
+                    } catch(FileNotFoundException e) {}
+                    Palette palette = Palette.from(image).generate();
+                    this.getWindow().getDecorView().setBackgroundColor(palette.getVibrantColor(0));
+                }
+                else
+                {
+                    throw new NullPointerException();
+                }
+            }
+        }
     }
 }
 
