f48e6e91df50ac52236dfe706412609e5d74d847
w3tian <w3tian@uwaterloo.ca>
2017-03-25 16:47:51 -0400

    bla

--- server/search.py
+++ server/search.py
@@ -1,7 +1,6 @@
+#!/usr/bin/env python2
 import requests
 import json
 
-client_id="851491bcca225c2e3dde"
-client_secret="7787437fafe08a29651ef8946b772839a08785dd"
-data = requests.get(url="https://${client_id}:${client_secret}@api.shutterstock.com/v2/images/search?query=food")
+data = requests.get(url="https://851491bcca225c2e3dde:7787437fafe08a29651ef8946b772839a08785dd@api.shutterstock.com/v2/images/search?query=food")
 print(data.json())
